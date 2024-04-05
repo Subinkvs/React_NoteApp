63ec884772949faa01286908c5182028210f2172
SubinSundar <subinsundar9624@gmail.com>
2024-04-05 11:25:13 +0530

    added router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import './App.css';
 import Header from './components/Header';
 import NotesListPages from './pages/NotesListPages';
+import NotePage from "./pages/NotePage";
 
 function App() {
   return (
-    <div>
-      <Header></Header>
-      <NotesListPages></NotesListPages>
-     </div>
-    );
+    <Router>
+      <div>
+        <Header />
+        <Routes>
+          <Route path='/' exact element={<NotesListPages />} />
+          <Route path='/note' element={<NotePage />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
+
 export default App;
+
---
+++ src/pages/NotePage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function NotePage() {
+  return (
+    <div>
+      <h2 style={{color:'red'}}>
+        This is a new page
+      </h2>
+    </div>
+  )
+}
+
+export default NotePage
